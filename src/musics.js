e5732f1171e9de012ae43f5c0c8b60900c93c359
BayramBehbudov <bayram.behbudov@gmail.com>
2024-06-21 19:07:51 +0400

    first commit

---
+++ src/musics.js
@@ -0,0 +1,40 @@
+export default [{
+    name: "Bir Könül Sındırmışam",
+    singer: "Şövkət Ələkbərova",
+    url: "../music/bir-konul-sindirmisam.m4a",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/37/f9/0e/37f90e69-b279-f536-a62a-c2c058b3f021/cover.jpg/1200x1200bf-60.jpg",
+    isLiked: false,
+
+},
+{
+    name: "Məktəb illəri",
+    singer: "Lalə Məmmədova",
+    url: "../music/mekteb-illeri.m4a",
+    cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6On3swq0ZPkx9Atqa900rZ2AcqM5l_d_3kLq68WGXLT1bCzvMRBVoSI2Gnjf-Gvl-EVs&usqp=CAU",
+    isLiked: false,
+}
+    ,
+{
+    name: "Dance Me to the End of Love",
+    singer: "Leonard Cohen",
+    url: "../music/leonard.m4a",
+    cover: "https://i1.sndcdn.com/artworks-000660711277-domwf4-t500x500.jpg",
+    isLiked: false,
+},
+
+{
+    name: "Cola Song",
+    singer: "INNA",
+    url: "../music/inna-cola-song.m4a",
+    cover: "https://i1.sndcdn.com/artworks-000087162906-fszb4d-t500x500.jpg",
+    isLiked: false,
+}
+    ,
+{
+    name: "World Dance",
+    singer: "Yanni",
+    url: "../music/yanni.m4a",
+    cover: "https://i1.sndcdn.com/artworks-000126949875-k8ddy5-t500x500.jpg",
+    isLiked: false,
+}
+]
