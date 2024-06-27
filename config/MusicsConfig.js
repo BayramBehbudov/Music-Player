6404ed80426271019dc85c1acaa164b2846cd5d5
BayramBehbudov <bayram.behbudov@gmail.com>
2024-06-27 23:21:43 +0400

    api control

--- config/MusicsConfig.js
+++ config/MusicsConfig.js
@@ -1,35 +1,16 @@
-// const myHeaders = new Headers();
-
-// myHeaders.append("Content-Type", "application/json");
-
-// const requestOptions = {
-//   method: "get",
-//   headers: myHeaders,
-//   redirect: "follow",
-// };
-
-// export default await fetch(
-//   `https://v1.nocodeapi.com/bayram/spotify/fnLVfKwgeFMFqkuF/search?q=<q>&type=track&perPage=20&page=1`,
-//   requestOptions
-// )
-//   .then((response) => response.json())
-//   .then((result) => result.tracks.items)
-//   .catch((error) => console.log("error", error));
-
-
 const fetchMusicData = async () => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
   const requestOptions = {
-    method: "get",
+    method: "GET",
     headers: myHeaders,
     redirect: "follow",
   };
 
   try {
     const response = await fetch(
-      `https://v1.nocodeapi.com/bayram/spotify/fnLVfKwgeFMFqkuF/search?q=<q>&type=track&perPage=20&page=1`,
+      `https://v1.nocodeapi.com/musicsplayer/spotify/VAfDlWnvkiSoRjPy/search?q=<q>&type=track&perPage=20&page=1`,
       requestOptions
     );
     const result = await response.json();
